44e29bb83b7da1fb787a21dc04eb881e7305a0af
Said ALsalti <firist821@gmail.com>
2024-12-03 09:34:44 +0400

    اضافة صفحة بسيطة لا ضافة صنف

--- Store1/Data/Dtos/CategoryDto.cs
+++ Store1/Data/Dtos/CategoryDto.cs
@@ -1,4 +1,7 @@
-﻿namespace Store1.Data.Dtos
+﻿using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+
+namespace Store1.Data.Dtos
 {
     public class CategoryDto
     {
@@ -6,7 +9,9 @@
         public string? NameAr { get; set; }
         public string? ImagePath { get; set; }
         public int? OrderIndex { get; set; }
+        [Label("فعّال")]
         public bool? IsActive { get; set; }
-       
+        public IBrowserFile?   file { get; set; }
+
     }
 }
